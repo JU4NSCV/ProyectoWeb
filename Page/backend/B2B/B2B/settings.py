0c51662bbb2982cb782aeb2339ebc48019be1318
JU4NSCV <jscueva5@utpl.edu.ec>
2026-07-29 23:49:32 -0500

    POST Produccion

--- Page/backend/B2B/B2B/settings.py
+++ Page/backend/B2B/B2B/settings.py
@@ -144,6 +144,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# Directorio donde Django recolectará los estáticos (necesario para Render/WhiteNoise)
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
